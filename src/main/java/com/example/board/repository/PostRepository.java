1ddf0de2278c29d97dcce745dc1324c6385b47c7
haru <chocomos_tea86@ezweb.ne.jp>
2025-05-31 15:40:17 +0900

    #1 19掲示板の機能追加

--- src/main/java/com/example/board/repository/PostRepository.java
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.example.board.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.board.entity.Post;
@@ -9,4 +11,11 @@ import com.example.board.entity.Post;
  */
 
 public interface PostRepository extends JpaRepository<Post, String> {
+	/**
+	* IDで検索する
+	*
+	* @param id ID
+	* @return 投稿
+	*/
+	public Optional<Post> findById(String id);
 }
